002f5eb53b0dc6b2923aeb8be7f1b71f3e6f00ce
Ashwin Pai <apai2@wpi.edu>
2021-03-13 14:57:15 -0500

    Worldmap is now visible via index.html
    1. Created direc for different file type for clarity
    2. Script for world map is in worldmap.js file.

---
+++ WorldMap/scripts/worldmap.js
@@ -0,0 +1,48 @@
+
+    // Constants
+    const width = 2000;
+    const height = 600;
+    const margin = {top: 10, right: 5, bottom: 15, left: 20},
+        svgWidth = width - margin.left - margin.right,
+        svgHeight = height - margin.top - margin.bottom;
+
+    //Create an SVG
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight)
+        .attr("class", "svgMap")
+
+
+    //Import the geojson for the world
+    Promise.all([
+        d3.json('geojson/worldmap.geo.json')
+    ]).then( ([countries]) => {
+        createMap(countries);
+    })
+
+    /**
+     * The function which creates the worldmap from the geo.json file.
+     *
+     * @param countries
+     */
+    function createMap(countries){
+
+        const projection = d3.geoCylindricalStereographic()
+            .scale(200);
+
+        const geoPath = d3.geoPath()
+            .projection(projection)
+
+
+        svg.selectAll('path')
+            .data(countries.features)
+            .enter()
+            .append('path')
+            .attr('d', function(d){return geoPath(d)})
+            .attr('stroke-width', 1)
+            .attr('stroke', '#252525')
+            .attr('class', function(d){return d.properties.name})
+            .attr('fill', '#FFFFFF')
+            .attr('transform', 'translate(' + svgWidth/4.5 + ',' + 100 + ')')
+    }
