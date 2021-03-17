a163ca1d2e39b390f3c1b7902fc20020cfadb614
Ashwin Pai <apai2@wpi.edu>
2021-03-17 14:46:12 -0400

    Fixed Window Sizing and Added nav bar to proper div element

--- scripts/worldmap.js
+++ scripts/worldmap.js
@@ -3,7 +3,7 @@
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight * (0.6);
 var width = windowWidth;
-var height = windowHeight;
+var height = windowHeight - 90;
 const scale = 300;
 const margin = {top: 0, right: 0, bottom: 0, left: 0},
     svgWidth = width - margin.left - margin.right,
